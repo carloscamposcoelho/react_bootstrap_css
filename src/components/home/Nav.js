98d6623e927c1050a8a792c5435a059c32d557b8
carlos.coelho <carlos.coelho@inspira.com.br>
2021-04-30 18:56:08 -0300

    Navbar stylization 1

--- src/components/home/Nav.js
+++ src/components/home/Nav.js
@@ -1,28 +1,34 @@
-import React, { Component } from "react";
 
-export default class Nav extends Component {
-    render() {
-
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <span class="navbar-brand mb-0 h1">Navbar</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href="#features">Features</a>
-                            <a className="nav-link" href="#about">About</a>
-                            <a className="nav-link" href="#services">Services</a>
-                            <a className="nav-link" href="#gallery">Gallery</a>
-                            <a className="nav-link" href="#testimonials">Testimonials</a>
-                            <a className="nav-link" href="#team">Team</a>
-                            <a className="nav-link" href="#contact">Contact</a>
-                        </div>
+export default function Nav () {
+    return (
+        <>
+        <nav id="menu" className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Navbar</span>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="#features">Features</a>
+                        <a className="nav-link" href="#about">About</a>
+                        <a className="nav-link" href="#services">Services</a>
+                        <a className="nav-link" href="#gallery">Gallery</a>
+                        <a className="nav-link" href="#testimonials">Testimonials</a>
+                        <a className="nav-link" href="#team">Team</a>
+                        <a className="nav-link" href="#contact">Contact</a>
                     </div>
                 </div>
-            </nav>
-        )
-    }
+            </div>
+        </nav>
+        <div id="about" style={{height:"100vh"}}>
+            <h1> About</h1>
+
+        </div>
+        <div id="services" style={{height:"100vh"}}>
+            <h1> Services</h1>
+
+        </div>
+        </>
+    )
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import  {BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import Home from './layouts/Home';
 
+import "./assets/css/style.css";
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 ReactDOM.render(
   <React.StrictMode>
